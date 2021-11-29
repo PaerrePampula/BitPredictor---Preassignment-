c6d422c0862ea324bb5de3aa3f7693ce1d7c34d8
Patrik Kumpula <patrik19100@student.hamk.fi>
2021-11-29 18:47:53 +0200

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+/* Set the width of the side navigation to 250px */
+function opensideNavigation() {
+    document.getElementById("sidenavigation").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closesideNavigation() {
+    document.getElementById("sidenavigation").style.width = "0";
+}
